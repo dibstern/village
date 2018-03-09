bde96c304c86b03dd28a20d686489aa86332fd76
nguyen.hoamy <nguyen.hoamy@accenture.com>
2018-03-09 16:09:49 +0100

    Add log-in form

--- src/components/CardHolder.js
+++ src/components/CardHolder.js
@@ -1,18 +1,24 @@
     import React from 'react';
     import Card from './Card';
     import Carousel from './Carousel';
-    import {Button, Grid, Row, Col} from 'react-bootstrap';
     import './css/CardHolder.css';
     import redCrossImg from '../img/red_cross.png';
     import Title from './Title';
-import Paragraph from './Paragraph';
-
+    import Paragraph from './Paragraph';
+    import SignUpButton from './SignUpButton';
+    
     const CardHolder = () => (
       <div className="body">
-          <Title text = "Build Your Village"/>
-          <Paragraph text = "Find a local buddy."/>
-          <Paragraph text = "Get involved with the community." />
-    </div>
-    );
+      <Title text = "Build Your Village"/>
+      <Paragraph text = "Find a local buddy."/>
+      <Paragraph text = "Get involved with the community." />
 
+      <div className = "buttonStyle">
+            <SignUpButton/>
+      </div>
+      </div>
+      
+    );
+    
     export default CardHolder;
+    
---
+++ src/components/SignUpButton.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './css/Card.css';
+import { Button } from 'react-bootstrap';
+import LogInForm from './LogInForm';
+
+const wellStyles = { maxWidth: 400, margin: '0 auto 10px' };
+
+class SignUpButton extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        showReply: false
+      }
+    }
+    onClick(e){
+      e.preventDefault();
+      this.setState({showReply: !this.state.showReply})
+    }
+    render() {
+      return (
+  <div style={wellStyles}>
+    <Button bsStyle="warning" bsSize="large" block>
+      Sign Up
+    </Button>
+    <br/>
+    <a onClick={this.onClick.bind(this)} href='#'>
+    <Button bsSize="large" block>
+      Log In
+    </Button>
+    </a>
+    <br/>
+    {this.state.showReply && <LogInForm/>}
+  </div>
+)
+    }
+};
+
+export default SignUpButton;
