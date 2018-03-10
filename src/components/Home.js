2f519964e1bb4052f903050ab25f861eb538f9d6
Unknown <dibstern@gmail.com>
2018-03-10 05:27:22 +0100

    Add Routing

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import VillageNavBar from './VillageNavBar';
+import SimpleSlider from './Slider';
+import MyFancyComponent from './Map';
+import ProgressInstance from './ProgressBar';
+import ServiceHolder from './ServiceHolder';
+
+const Home = () => (
+  <div>
+    <VillageNavBar id="home"/>
+    <br/>
+    <ProgressInstance/>
+    <br/>
+    <SimpleSlider/>
+    <br/>
+    <MyFancyComponent
+  googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBFM4v4ZB4gYCWukuBHF0rv0rTVl5gn40M&v=3.exp&libraries=geometry,drawing,places"
+  loadingElement={<div style={{ height: `100%` }} />}
+  containerElement={<div style={{ height: `400px` }} />}
+  mapElement={<div style={{ height: `100%` }} />}/>
+    <br/>
+    <ServiceHolder/>
+    {/*
+    To use bootstrap components see : https://react-bootstrap.github.io/components/popovers/ 
+    First import the component from react-bootstrap
+      import { Button } from 'react-bootstrap';
+    */}
+  </div>
+);
+
+export default Home;
--- src/components/VillageNavBar.js
+++ src/components/VillageNavBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Input, Menu } from 'semantic-ui-react'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import 'semantic-ui-css/semantic.min.css';
 
 
@@ -13,14 +14,18 @@ export default class MenuExampleSecondary extends Component {
 
     return (
       <Menu secondary>
-        <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+        <Link to="/home">
+          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+        </Link>
         <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} />
         <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick} />
         <Menu.Menu position='right'>
           <Menu.Item>
             <Input icon='search' placeholder='Search...' />
           </Menu.Item>
-          <Menu.Item name='logout' active={activeItem === 'logout'} onClick={this.handleItemClick} />
+          <Link to="/login">
+            <Menu.Item name='logout' active={activeItem === 'logout'} onClick={this.handleItemClick} />
+          </Link>
         </Menu.Menu>
       </Menu>
     )
