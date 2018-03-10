89e4362feb240006830c6f0ae5a12706073449b0
Bishal Sapkota <sapkotab@student.unimelb.edu.au>
2018-03-10 05:25:18 +0100

    Add my village

---
+++ public/my_village/vendor/bishal/formHandler.js
@@ -0,0 +1,36 @@
+$('.flagSelector').click(function() {
+    var country = $(this).attr('data-country');
+    // alert(country);
+    $("#countryPanel").hide();
+    $("#countryTextbox").val(country);
+    $("#languagePanel").show();
+});
+
+$('.languageSelector').click(function() {
+    var language = $(this).attr('data-language');
+    $("#languagePanel").hide();
+    $("#languageTextbox").val(language);
+    $("#registrationPanel").show();
+});
+
+
+$('#signupButton').click(function(){
+    var country = $("#countryTextbox").val();
+    var language = $("#languageTextbox").val();
+    var name = $("#name").val();
+    var username = $("#username").val();
+    var password = $("#password").val();
+    var city = $("#city").val();
+    var payload = {'country': country, 'language': language, 'name': name, 'username': username, 'password': password, 'city': city};
+    console.log(payload)
+    var url = 'http://127.0.0.1:8000/profile';
+    var datatype = 'application/json';
+
+    // perform fucking ajax
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: payload,
+        success: function(data) {alert(JSON.stringify(data))}
+      });
+});
